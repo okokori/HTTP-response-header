4456bcbda78e102eac824ced13e296fa5ae1369f
x3chs <onbal72@gmail.com>
2023-09-06 19:24:08 +0300

    Update check_headers.py

--- check_headers.py
+++ check_headers.py
@@ -2,26 +2,34 @@ import requests
 
 def check_headers(headers):
     missing_headers = []
+    highlighted_headers = []
     for header in ['X-Frame-Options', 'X-XSS-Protection', 'Content-Security-Policy']:
         if header not in headers:
             missing_headers.append(header)
-    return missing_headers
+        else:
+            highlighted_headers.append(header)
+    return missing_headers, highlighted_headers
 
 def suggest_solutions(missing_headers):
     solutions = {}
+    red_color = "\033[91m"
+    reset_color = "\033[0m"
     for header in missing_headers:
         if header == 'X-Frame-Options':
-            solutions[header] = "X-Frame-Options headerını ekleyin ve değeri 'DENY' olarak ayarlayın."
+            solutions[header] = f"{red_color}X-Frame-Options headerını ekleyin ve değeri 'DENY' olarak ayarlayın.{reset_color}"
         elif header == 'X-XSS-Protection':
-            solutions[header] = "X-XSS-Protection headerını ekleyin ve değeri '1; mode=block' olarak ayarlayın."
+            solutions[header] = f"{red_color}X-XSS-Protection headerını ekleyin ve değeri '1; mode=block' olarak ayarlayın.{reset_color}"
         elif header == 'Content-Security-Policy':
-            solutions[header] = "Content-Security-Policy headerını ekleyin ve gerekli politikaları belirleyin."
+            solutions[header] = f"{red_color}Content-Security-Policy headerını ekleyin ve gerekli politikaları belirleyin.{reset_color}"
     return solutions
 
-def show_results(headers, solutions):
+def show_results(headers, solutions, highlighted_headers):
     print("Header Kontrol Sonuçları:\n")
     for header, value in headers.items():
-        print(f"{header}: {value}")
+        if header in highlighted_headers:
+            print(f"\033[93m{header}: {value}\033[0m")
+        else:
+            print(f"{header}: {value}")
     print("\nÇözüm Önerileri:\n")
     for header, solution in solutions.items():
         print(f"{header} için çözüm önerisi: {solution}")
@@ -30,10 +38,9 @@ def main():
     target = input("Hedef domain veya IP adresini girin: ")
     response = requests.get(target)
     headers = response.headers
-    headers['HTTP Durumu'] = response.status_code
-    missing_headers = check_headers(headers)
+    missing_headers, highlighted_headers = check_headers(headers)
     solutions = suggest_solutions(missing_headers)
-    show_results(headers, solutions)
+    show_results(headers, solutions, highlighted_headers)
 
 if __name__ == "__main__":
     main()
